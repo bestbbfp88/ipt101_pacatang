724f5aaba315ab3ec89a95964437aaab7ec6006f
Biancent Pacatang <biancentpacatang3@gmail.com>
2023-03-28 11:56:44 +0800

    Edited File

--- PHP/pacatang.php
+++ PHP/pacatang.php
@@ -5,14 +5,24 @@
         background-color: antiquewhite;
         color: black;
     }
+
+    li{
+        border:solid;
+        width:10%;
+    }
+    .title{
+        font-size: 50px;
+    }
 </style>
 
 </head>
 
 
     <body>
+        <div class = "title">
+            MULTIPLICATION TABLE
+        </div>
             <?php
-            echo "Biancent F. Pacatang";
             $number = 5;
             echo "<ul style='list-style: none;' > ";
             for($i = 1; $i <= 10; $i++){
